ef671a43bf463dd5a61bc3938931fd3075a569a0
Jonathan S.P <jonhnspa@gmail.com>
2025-02-23 10:29:14 +0100

    Crear Header y Footer

--- practica2-react/src/App.jsx
+++ practica2-react/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-
+import Header from './componentes/ui/Header.jsx'
+import Footer from './componentes/ui/Footer.jsx'
 function App() {
-  
+
 
   return (
-    <>
-     
-    </>
+    <div>
+      <Header />
+      <Footer />
+    </div>
+
+
   )
+
 }
 
 export default App
